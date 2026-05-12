d39c6b27b0e5499da4d580f9e7f7a247c1b68039
abdi-ba <abdiabebe7323@gmail.com>
2026-05-12 12:35:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="abdi-check",
     version="1.0",
-    py_modules=["abdi"],  
+    py_modules=["abdi"],
     entry_points={
         'console_scripts': [
-            'abdi-check=abdi:main', 
+            'abdi-check=abdi:main',
         ],
     },
 )
